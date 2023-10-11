9fbdf77506333f30edc99a4dec1503254e13bbc7
williamtanuwijaya <will.tw0807@gmail.com>
2023-10-11 22:54:10 +0700

    update

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ProductCard from './ProductCard';
+import Image from 'next/image';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 const productsData = [
   {
@@ -32,14 +33,30 @@ const productsData = [
   },
 ];
 
+const generateRating = (rating) => {
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars.push(<AiFillStar key={i} className="text-[#FF9529]" />);
+    } else {
+      stars.push(<AiOutlineStar key={i} className="text-[#FF9529]" />);
+    }
+  }
+  return <div className="flex gap-1 text-[20px]">{stars}</div>;
+};
+
 export default function NewProducts() {
   return (
     <div>
       <div className="container pt-16">
         <h2 className="font-medium text-2xl pb-4 text-center">New Products</h2>
-        <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
-          {productsData.map((item, index) => (
-            <ProductCard key={index} img={item.img} title={item.title} desc={item.desc} rating={item.rating} price={item.price} />
+        <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
+          {productsData.map((product, index) => (
+            <div key={index} className="px-4 border border-gray-200 rounded-xl max-w-[400px]">
+              <div>
+                <Image src={product.img} width={500} height={600} alt={product.title} />
+              </div>
+            </div>
           ))}
         </div>
       </div>
