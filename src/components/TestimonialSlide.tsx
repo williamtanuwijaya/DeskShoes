4c8aaa35f15f69ed8969321b2c05629180ed52fd
Umar Muhdhor <muhdhorcs@gmail.com>
2023-10-12 23:31:50 +0700

    Finishing

--- src/components/TestimonialSlide.tsx
+++ src/components/TestimonialSlide.tsx
@@ -5,10 +5,10 @@ const TestimonialSlide = ({ name, position, image, quote }) => {
   return (
     <div className="border border-gray-300 rounded-2xl p-6 lg:p-0 m-5">
       <div className="text-center flex flex-col items-center gap-1">
-        <Image className="rounded-full inline-block" src={image} width={80} height={80} alt="dp" />
+        <Image className="border border-slate-950 m-5 rounded-full inline-block" src={image} width={80} height={80} alt="dp" />
         <h2 className="text-gray-500 font-black text-[20px]">{name}</h2>
         <p>{position}</p>
-        <p className="max-w-[200px] text-gray-500">{quote}</p>
+        <p className="max-w-[1000px] m-5 mt-2 text-gray-500">{quote}</p>
       </div>
     </div>
   );
